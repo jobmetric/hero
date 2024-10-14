8b4610c95a9307bfa7c0188513a55aa3df3482f6
majidmohammadian <majeedmohammadian@gmail.com>
2024-10-14 19:47:30 +0330

    added language key

---
+++ lang/en/base.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Base Hero Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during Hero for
+    | various messages that we need to display to the user.
+    |
+    */
+
+    'description' => 'A powerful and intuitive admin panel for managing your application with ease and efficiency.',
+
+    'sections' => [
+        'content' => [
+            'name' => 'Content',
+            'title' => 'Content',
+            'menus' => [
+                'products' => 'Products',
+            ],
+        ],
+    ],
+
+];
---
+++ lang/fa/base.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Base Hero Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during Hero for
+    | various messages that we need to display to the user.
+    |
+    */
+
+    'description' => 'یک پنل مدیریت قدرتمند و بصری برای مدیریت آسان و کارآمد برنامه شما.',
+
+];
